787c249c8b4381d2b0b5d8e8229cac01cd19e435
Robert Ricci <ricci@cs.utah.edu>
2026-06-18 22:22:08 -0600

    Revert "Create jazz_ensemble.py"

--- src/jazz_ensemble.py
+++
@@ -1,9 +0,0 @@
-from jazz_goblin import JazzGoblin
-
-vocalist = JazzGoblin()
-trumpeter = JazzGoblin(rhythm=True, music=True, my_man=True)
-not_trumpeter = JazzGoblin()
-
-vocalist.skiddily_bop_bop_ba_woo_sham_boo()
-trumpeter.trumpet_solo(trumpet=True)
-not_trumpeter.trumpet_solo()
